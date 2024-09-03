356380cd9397d634841ab8339b19d71ae9d20990
yenin-C <misscurlyblacks@myyahoo.com>
2024-09-03 13:36:57 -0400

    Add files via upload

---
+++ Ex1_Output.py
@@ -0,0 +1,6 @@
+# Intro Examples
+# using print() function to display
+
+print("Hello Yenin")
+print("Hello python Class")
+print("Hello ET574")
---
+++ Ex2_Input.py
@@ -0,0 +1,6 @@
+# Intro Examples
+# using input() & print() function to display
+
+course  = input("Enter your course :")
+print("Welcome to " + course + '!')
+print("Hi ET574 class")

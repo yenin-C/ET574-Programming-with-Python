d62bcbf0050887c396a2eac014fd624e6931cc3a
yenin-C <misscurlyblacks@myyahoo.com>
2024-09-12 13:52:12 -0400

    Add files via upload

---
+++ PE2_2.py
@@ -0,0 +1,17 @@
+#Part A
+#0.05 = interest   <---- Incorrect ('interest' should be the variable and not the number)
+interest = 0.05
+balance = 10
+print(interest*balance)
+
+#Part B
+bal = 123
+#dep = $100 <---- Incorrect (dollar sign has to be removed)
+dep = 100
+print(bal+dep)
+
+#Part C
+a = 2
+b = 3
+#a+b = c <----- Incorrect 
+print(c)

f962b1a562aee0391435e896d10b13e06e47dd60
yenin-C <misscurlyblacks@myyahoo.com>
2024-09-14 13:00:39 -0400

    Add files via upload

---
+++ PE2_3.py
@@ -0,0 +1,13 @@
+greet = 'welcome to a new semester!'
+print(greet.capitalize())
+
+first = 'yenin'
+last = 'carrion'
+name = first + ' ' + last
+print(name.title())
+
+class1 = 'eng101'
+class2 = 'ma114'
+class3 = 'et574'
+courses = class1 + '\t' + class2 + '\t' + class3
+print(courses.upper())

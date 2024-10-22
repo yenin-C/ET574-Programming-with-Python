cb92b63c26243e962bded6b20ba2f17b8af98051
yenin-C <misscurlyblacks@myyahoo.com>
2024-10-22 13:20:58 -0400

    Update PE06_.py

--- PE06_.py
+++ PE06_.py
@@ -18,3 +18,22 @@ elif age >= 65:
     print("You're an elder")
 
 print()
+
+#Another example for No. 2
+
+age = int(input ("Please tell me your correct age:"))
+
+if age < 0:
+    print("Invalid age")
+elif age < 2:
+    print("You\'re a baby")
+elif age <= 4:
+    print("You\'re a toddler")
+elif age < 13:
+    print("You\'re a kid")
+elif age < 20:
+    print("You\'re a teenager")
+elif age < 65:
+    print("You\'re an adult")
+elif age <= 65:
+    print("You\'re an elder")

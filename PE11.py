3b5c4997d195b559d79da6f8bb771cfb4a73768c
yenin-C <misscurlyblacks@myyahoo.com>
2024-12-06 19:17:09 -0500

    Update PE11.py

--- PE11.py
+++ PE11.py
@@ -1,4 +1,15 @@
 '''
+1. Class Implementation
+a) Implement a class Rectangle with two attributes, width and height.
+b) Implement an init method with an optional parameter type.
+Set the default value of the attributes of width and height to 1.
+c) Implement a display method to print the values of width and height.
+d) Instantiate two objects of type rectangle, one with arguments one without.
+r1 = Rectangle(4, 5)
+r2 = Rectangle()
+e) Call display() to print width and height.
+f) Access and print the attribute values of r1 and r2.
+'''
 class Rectangle:
     def __init__(self, name=1, width=1, height=1):
         self.name = name
@@ -16,11 +27,26 @@ r2.display()
 
 print(r1.name, r1.width, r1.height)
 print(r2.name, r2.width, r2.height)
-'''
 
-from math import pi
+print('\n')
 
-class Circle():
-    Radius = 10
-    def __init__(self, radius=1):
-        self.radius = radius
+# 3
+# main.py
+from shapes import Circle, Rectangle
+
+circle = Circle()
+circle.setRadius(10)
+circle.display()
+print(f"Area: {circle.area()}")
+print(f"Circumference: {circle.circumference()}")
+
+print()
+
+rectangle = Rectangle()
+rectangle.display()
+rectangle.setLength(4)
+rectangle.setWidth(3)
+print(rectangle.getLength())
+print(rectangle.getWidth())
+print(rectangle.area())
+print(rectangle.perimeter())

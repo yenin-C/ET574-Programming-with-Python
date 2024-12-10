92cd8fa6768f6c3b94256a27b87cc14c99277406
yenin-C <misscurlyblacks@myyahoo.com>
2024-12-10 13:38:41 -0500

    Update PE12.py

--- PE12.py
+++ PE12.py
@@ -19,3 +19,28 @@ with open(filename) as file_object:
 for line in lines[:3]:  # Slice the list to get the first three lines
     print(line.rstrip())
 
+#2. File Writing
+#Write a program that defines two different functions to create files
+filename = 'Sample3.txt'
+with open(filename, 'x') as file_object:
+    file_object.write("George Washington\n")
+    file_object.write("John Adams\n")
+
+print('\n')
+
+#3. File Appending
+#Write a program that adds/appends lines to the end of an existing file
+filename = 'Sample3.txt'
+with open(filename, 'a') as file_object:
+    file_object.write("Thomas Jefferson.\n")
+    file_object.write("James Madison.\n")
+    file_object.write("James Monroe.\n")
+    file_object.write("John Quincy Adams.\n")
+#Open File Created
+filename = 'Sample3.txt'
+file_object = open(filename, 'r')
+#read() method: Returns the file content
+contents = file_object.read()
+print(contents)
+
+
